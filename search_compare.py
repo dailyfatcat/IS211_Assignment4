81fcbeb15718a6e86c1edd5b0337824e33e6c044
dailyfatcat <50534920+dailyfatcat@users.noreply.github.com>
2021-02-28 13:10:15 -0500

    Assigment4, update from announcements

--- search_compare.py
+++ search_compare.py
@@ -117,14 +117,14 @@ if __name__ == "__main__":
     searches = {"Sequential Search": sequential_search, "Ordered Sequential Search": ordered_sequential_search,
                 "Binary Search Iterative": binary_search_iterative, "Binary search recursive": binary_search_recursive}
 
-    list_size = [500, 1000, 10000]
+    list_size = [500, 1000, 5000]
 
     for search in searches:
         for lists in list_size:
             total_time = 0
             for i in range(100):
                 sorted_list = sorted(get_me_random_list(lists))
-                results = searches[search](sorted_list, -1)
+                results = searches[search](sorted_list, 99999999)
                 total_time += results[1]
             avg_time = total_time/100
             print(f"List Size:{lists}: {search} took {avg_time:10.7f} seconds to run on average")
